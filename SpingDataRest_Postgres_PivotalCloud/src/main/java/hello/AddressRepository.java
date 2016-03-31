153dc70af4941eb34828181f5b42b47c9a64a752
Vathsalya <vathsalya.b@gmail.com>
2016-03-31 16:53:56 -0500

    Deleted files

--- SpingDataRest_Postgres_PivotalCloud/src/main/java/hello/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "address", path = "address")
-public interface AddressRepository extends PagingAndSortingRepository<Address, Long> {
-
-}
